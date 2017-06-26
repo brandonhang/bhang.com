6f0351cb31be0e910678d11d6e43f434e41fbb40
Brandon Hang <brandonshang@gmail.com>
2017-06-26 01:21:33 -0400

    Nav bar

--- js/bhangular.js
+++ js/bhangular.js
@@ -1,20 +1,43 @@
 (function() {
     var app = angular.module('bhang-app', []);
-    
+
     app.controller('bhang-ctrl', function($scope, $http) {
         $http.get('./config/nav.json').then(function(navData) {
             $scope.navData = navData.data;
         });
-    });
-    
-    app.controller('personal-ctrl', function($scope, $http) {
-        $http.get('./config/about.txt').then(function(aboutData) {
-            $scope.aboutLines = aboutData.data.split('\n');
-        });
-    });
-    app.controller('personal-ctrl', function($scope, $http) {
-        $http.get('./config/cars.json').then(function(carsData) {
-            $scope.carsData = carsData.data;
-        });
+
+        //////////////// Events ////////////////
+        $scope.navMenu = {};
+
+        $scope.openMainMenu = function($event) {
+            if (!('main' in $scope.navMenu)) {
+                $scope.navMenu.main = false;
+            }
+
+            $scope.navMenu.main = !$scope.navMenu.main;
+
+            if (!$scope.navMenu.main) {
+                angular.forEach($scope.navMenu, function(group, groupName) {
+                    $scope.navMenu[groupName] = false;
+                });
+            }
+
+            $event.preventDefault();
+        }
+
+        $scope.openSubMenu = function($event, group) {
+            if (!(group in $scope.navMenu)) {
+                $scope.navMenu[group] = false;
+            }
+
+            $scope.navMenu[group] = !$scope.navMenu[group];
+            $event.preventDefault();
+        }
+
+        $scope.closeMenu = function() {
+            angular.forEach($scope.navMenu, function(group, groupName) {
+                $scope.navMenu[groupName] = false;
+            });
+        }
     });
 })();
