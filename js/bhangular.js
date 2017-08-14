4182ba71220b57514d2eb570046cb3851d5ba8d9
brandonhang <brandonshang@gmail.com>
2017-08-13 23:22:57 -0400

    Updated splash page; clean working files

--- js/bhangular.js
+++ js/bhangular.js
@@ -1,82 +1,37 @@
 (function() {
     var app = angular.module('bhang-app', []);
 
-    app.controller('bhang-ctrl', function($scope, $http, $location, $anchorScroll) {
+    app.controller('bhang-ctrl', ['$scope', '$http', function($scope, $http) {
         $http.get('./config/nav.json').then(function(navData) {
             $scope.navData = navData.data;
+            $scope.navMenu = {};
+            $scope.mobileMenu = false;
+
+            angular.forEach($scope.navData, function(navGroup, group) {
+                $scope.navMenu[group] = false;
+            });
         });
 
-        $http.get('./config/splash.txt').then(function(splashData) {
-            var splashLines = splashData.data.split('\n');
-
-            $scope.splashData = {};
-            $scope.splashData.caption = splashLines[0];
-            $scope.splashData.abridgedBio = splashLines[1];
-        });
-
-        $scope.activeSubMenuName = '';
-        $scope.activeView = 'splash';
-        $scope.splashView = 'main';
-
-        //////////////// Events ////////////////
         $scope.openSubMenu = function($event, group) {
-            if (group == $scope.activeSubMenuName) {
-                $scope.activeSubMenuName = '';
-            }
-            else {
-                $scope.activeSubMenuName = group;
-                $scope.subMenuLinks = $scope.navData[group].links;
-            }
-
             $event.preventDefault();
-        }
-
-        $scope.closeMenu = function($event) {
-            $scope.activeSubMenuName = '';
-            $event.preventDefault();
-        }
-
-        $scope.navigate = function($event, target) {
-            console.log('Location: ' + $location.path());
-            console.log('Target: ' + target);
-            console.log('Do something, damnit');
-            if ($location.path() == target) {
-                console.log('You\'re already home!');
-            }
-            else {
-                var url = $location.path();
-                var base = url.split('#');
-                var navTo = target.split('#');
-
-                if (base[0] == navTo[0]) {
-                    // Smooth scroll here
+            angular.forEach($scope.navMenu, function(status, navGroup) {
+                if (group === navGroup) {
+                    $scope.navMenu[navGroup] = !$scope.navMenu[navGroup];
                 }
                 else {
-                    window.location.href = target;
+                    $scope.navMenu[navGroup] = false;
                 }
-                console.log(target);
-            }
-
+            });
+        }
+        $scope.closeMenu = function($event) {
             $event.preventDefault();
+            angular.forEach($scope.navMenu, function(status, navGroup) {
+                $scope.navMenu[navGroup] = false;
+            });
         }
-    });
-
-    //////////////// Filters ////////////////
-    app.filter('capitalize', function() {
-        return function(str) {
-            return str.charAt(0).toUpperCase() + str.slice(1);
+        $scope.openMainMenu = function($event) {
+            $event.preventDefault();
+            $scope.mobileMenu = !$scope.mobileMenu;
         }
-    });
-
-    app.filter('escape', function($sce) {
-        return function(str) {
-            return $sce.trustAsHtml(str);
-        }
-    });
-
-    //////////////// Beautify URLs ////////////////
-    app.config(['$locationProvider', function($locationProvider) {
-        $locationProvider.hashPrefix('');
-        $locationProvider.html5Mode(true);
     }]);
 })();
