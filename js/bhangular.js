5351e3a3f6136a75e31bc8b952c0a72b6460bc50
brandonhang <brandonshang@gmail.com>
2017-08-20 11:41:24 -0400

    About page mostly finished

--- js/bhangular.js
+++ js/bhangular.js
@@ -6,6 +6,7 @@
             $scope.navData = navData.data;
             $scope.navMenu = {};
             $scope.mobileMenu = false;
+            $scope.lightbox = false;
 
             angular.forEach($scope.navData, function(navGroup, group) {
                 $scope.navMenu[group] = false;
@@ -32,6 +33,22 @@
         $scope.openMainMenu = function($event) {
             $event.preventDefault();
             $scope.mobileMenu = !$scope.mobileMenu;
+        }
+        $scope.showPicture = function($event, picture) {
+            $event.preventDefault();
+            var lightbox = document.getElementById('lightbox');
+
+            if (lightbox !== null) {
+                var lightboxImg = document.getElementById('lightbox-image');
+                lightboxImg.style.backgroundImage = "url('/img/content/" + picture + "')";
+                $scope.lightbox = true;
+            }
+            else {
+                $scope.lightbox = false;
+            }
+        }
+        $scope.closeLightbox = function() {
+            $scope.lightbox = false;
         }
     }]);
 })();
