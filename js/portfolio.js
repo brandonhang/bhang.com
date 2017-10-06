8a6a0b09966f85bf26397afa55c8b53df07b8910
brandonhang <brandonshang@gmail.com>
2017-10-06 01:18:43 -0400

    Starting portfolio section with flip icons

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,6 @@
 (function() {
-    var portfolioApp = angular.module('portfolio-app', []);
-    portfolioApp.controller('portfolio-ctrl', ['$scope', '$http', function($scope, $http) {
+    var portfolioApp = angular.module('portfolio-app', ['ngSanitize']);
+    portfolioApp.controller('portfolio-ctrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
         // $http.get('/php/github.php').then(
         //     function(gitData) {
         //         $scope.numRepos = gitData.data.numRepos;
@@ -13,10 +13,30 @@
         //     function(gitErr) {
         //     }
         // );
-        $scope.flipSquare = function(event, index) {
-            if (event.target.tagName !== 'A') {
-                $scope.flippers[index] = !$scope.flippers[index];
+        $http.get('/config/projects.json').then(
+            function(projectData) {
+                $scope.projects = projectData.data;
+                $scope.flippers = Array.apply(null, Array($scope.projects.main.length +
+                    $scope.projects.school.length)).map(Boolean.prototype.valueOf, false);
+            },
+            function(projectErr) {
             }
+        );
+        $scope.flipSquare = function(event, index, isMain) {
+            var mainMod = isMain ? 0 : $scope.projects.main.length;
+
+            if (event.target.tagName !== 'A') {
+                $scope.flippers[index + mainMod] = !$scope.flippers[index + mainMod];
+            }
+        };
+        $scope.getDescription = function(index, isMain) {
+            try {
+                return isMain ? $scope.projects.main[index].desc : $scope.projects.school[index].desc;
+            }
+            catch (exc) {}
+        };
+        $scope.htmlDescription = function(index, isMain) {
+            return $sce.trustAsHtml($scope.getDescription(index, isMain));
         };
 
         //////// TEMP DATA ////////
@@ -26,34 +46,6 @@
         $scope.username = 'brandonhang';
         $scope.company = 'Management Sciences Associates, Inc.';
         $scope.location = 'Pittsburgh, PA';
-
-        $scope.projects = [
-            {
-                title: 'Subaru Impreza',
-                link: 'https://en.wikipedia.org/wiki/Subaru_Impreza#First_generation_.281992.E2.80.932000.29',
-                desc: 'First generation Subaru Impreza. Announced in October 1992.',
-                image: '/img/bkgrds/impreza-1000.jpg'
-            },
-            {
-                title: 'Blue Rondo à la Turk',
-                link: 'https://en.wikipedia.org/wiki/Subaru_Impreza#First_generation_.281992.E2.80.932000.29',
-                desc: 'Composed by Dave Brubeck. Part of the Time Out album.',
-                image: '/img/bkgrds/bluerondo-1000.jpg'
-            },
-            {
-                title: 'Ford GT40',
-                link: 'https://en.wikipedia.org/wiki/Ford_GT40#Mk_III',
-                desc: 'Third revision of the 24 Hours of Le Mans-winning Ford GT40',
-                image: '/img/bkgrds/gt40-1000.jpg'
-            },
-            {
-                title: 'Busy Bumblebee',
-                link: 'https://en.wikipedia.org/wiki/Bumblebee',
-                desc: 'A bumblebee gathers pollen from a flowering red bud in spring',
-                image: '/img/bkgrds/springtime-1000.jpg'
-            },
-        ];
-        $scope.flippers = Array.apply(null, Array($scope.projects.length)).map(Boolean.prototype.valueOf, false);
     }]);
 
     angular.bootstrap(document.getElementById('portfolio-app'), ['portfolio-app']);
